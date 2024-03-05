f5ba2e879d55fe6d1e46ce212e6c880355701486
Mehedi Hasan <127036046+Mehedi-16@users.noreply.github.com>
2024-03-05 23:45:52 +0600

    Add files via upload

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    int count = 0;
+    sort(s.begin(), s.end());
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != s[i + 1])
+        {
+            count++;
+        }
+    }
+    if (count % 2 == 0)
+    {
+        cout << "CHAT WITH HER!";
+    }
+    else
+    {
+        cout << "IGNORE HIM!";
+    }
+}
